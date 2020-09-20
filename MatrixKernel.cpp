585f8f8e47c9521a3cc783b3c0a4e7c37040624c
Ibrahim Hasan <ibrahim.hasan@mail.utoronto.ca>
2020-09-20 13:23:06 -0400

    Adds another small serial optimization

--- MatrixKernel.cpp
+++ MatrixKernel.cpp
@@ -23,10 +23,12 @@ int lsolveOptimized(int n, int *Lp, int *Li, double *Lx, double *x) {
   if (!Lp || !Li || !x)
     return (0); /* check inputs */
   for (j = 0; j < n; j++) {
-    x[j] /= Lx[Lp[j]];
-    const double x_j = x[j];
-    for (p = Lp[j] + 1; p < Lp[j + 1]; p++) {
-      x[Li[p]] -= Lx[p] * x_j;
+    if (x[j] != 0) {
+      x[j] /= Lx[Lp[j]];
+      const double x_j = x[j];
+      for (p = Lp[j] + 1; p < Lp[j + 1]; p++) {
+        x[Li[p]] -= Lx[p] * x_j;
+      }
     }
   }
   return (1);
